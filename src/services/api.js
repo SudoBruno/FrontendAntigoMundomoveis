e7a5205f91f47ce5e879b69c9923b67e9538df31
Pedro Dalpa <41978081+phdgamer@users.noreply.github.com>
2020-12-02 19:41:45 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://mudomoveisbackend.herokuapp.com/', //produção
-  baseURL: 'http://localhost:3333', //local
+  baseURL: 'https://mudomoveisbackend.herokuapp.com/', //produção
+//   baseURL: 'http://localhost:3333', //local
   // baseURL: 'https://mundomoveis.herokuapp.com/', //staging
 });
 
