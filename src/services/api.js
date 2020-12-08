713b41f3d57e628b13171e6070ebf3400f352b68
phdgamer <pedro380dalpa@gmail.com>
2020-12-08 09:17:04 -0300

    staging

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://mudomoveisbackend.herokuapp.com/', //produção
-  baseURL: 'http://localhost:3333', //local
-  // baseURL: 'https://mundomoveis.herokuapp.com/', //staging
+  // baseURL: 'http://localhost:3333', //local
+  baseURL: 'https://mundomoveis.herokuapp.com/', //staging
 });
 
 export default api;
