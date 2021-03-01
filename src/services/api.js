cc28f0e62c3e3e2bff4732f82bda6e1aa17835d3
phdgamer <pedro380dalpa@gmail.com>
2021-03-01 15:48:16 -0300

    add jwt and remove dependences

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const baseURL = `${process.env.REACT_APP_BASE_URL}`;
-
+console.log(Cookies.get('token'));
 const api = axios.create({
   baseURL,
+  headers: {
+    authorization: Cookies.get('token'),
+  },
 });
 
 export default api;
