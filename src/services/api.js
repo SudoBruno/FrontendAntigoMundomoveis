451dcdd69ad585f50c332f3eca5328da67072a31
phdgamer <pedro380dalpa@gmail.com>
2021-03-01 17:33:45 -0300

    git push

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 const baseURL = `${process.env.REACT_APP_BASE_URL}`;
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 
 const api = axios.create({
   baseURL,
-  headers: {
-    authorization: Cookies.get('token'),
-  },
+  // headers: {
+  //   authorization: Cookies.get('token'),
+  // },
 });
 
 export default api;
