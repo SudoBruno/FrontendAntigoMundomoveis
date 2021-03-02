0383851f8b346e715c233e954dd6a179d291d55a
phdgamer <pedro380dalpa@gmail.com>
2021-03-02 08:42:07 -0300

    het token in const

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 const baseURL = `${process.env.REACT_APP_BASE_URL}`;
-
+const auth = Cookies.get('token');
+console.log(auth);
 const api = axios.create({
   baseURL,
   headers: {
-    authorization: Cookies.get('token'),
+    authorization: auth,
   },
 });
 
