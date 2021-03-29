d9a272627aa0b46ca86f2e21be72726be8d4ce91
SudoBruno <brunoleonardo.profissional@gmail.com>
2021-03-29 09:05:11 -0300

    Paginação employee

--- src/pages/RH/Employee/index.js
+++ src/pages/RH/Employee/index.js
@@ -36,10 +36,7 @@ const Option = Select.Option;
 export default function Employee() {
   class SearchTable extends React.Component {
     state = {
-      pagination: {
-        current: 1,
-        pageSize: 10,
-      },
+      pagination: pagination,
       loading: false,
       searchText: '',
       searchedColumn: '',
@@ -126,16 +123,17 @@ export default function Employee() {
       this.setState({
         searchText: selectedKeys[0],
         searchedColumn: dataIndex,
+        pagination: pagination,
       });
     };
 
     handleReset = (clearFilters) => {
       clearFilters();
-      this.setState({ searchText: '' });
+      this.setState({ searchText: '', pagination: pagination });
     };
 
     handleTableChange = (pagination, filters, sorter) => {
-      this.fetch({
+      this.setState({
         sortField: sorter.field,
         sortOrder: sorter.order,
         pagination,
@@ -177,7 +175,10 @@ export default function Employee() {
               <React.Fragment>
                 <EditOutlined
                   style={{ cursor: 'pointer' }}
-                  onClick={() => handleEdit(record)}
+                  onClick={() => {
+                    handleEdit(record);
+                    setPagination(this.state.pagination);
+                  }}
                 />
                 {/* onClick={() => handleEdit(record)} */}
                 <Popconfirm
@@ -194,7 +195,12 @@ export default function Employee() {
         },
       ];
 
-      return <Table columns={columns} dataSource={employees} />;
+      return (
+        <Table columns={columns}
+          dataSource={employees}
+          onChange={this.handleTableChange}
+          pagination={this.state.pagination} />
+      );
     }
   }
 
@@ -241,6 +247,7 @@ export default function Employee() {
 
   const [secullumId, setSecullumId] = useState('');
   const [situation, setSituation] = useState('');
+  const [pagination, setPagination] = useState({ current: 1, pageSize: 10 });
 
   const data = {
     admission,
