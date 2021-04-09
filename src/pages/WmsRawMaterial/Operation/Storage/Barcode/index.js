19224e5673084e2da34c931901a7057035b231c0
phdgamer <pedro380dalpa@gmail.com>
2021-04-09 13:03:00 -0300

    alter wms tag layout

--- src/pages/WmsRawMaterial/Operation/Storage/Barcode/index.js
+++ src/pages/WmsRawMaterial/Operation/Storage/Barcode/index.js
@@ -20,99 +20,57 @@ function Barcode() {
   let content = [];
   for (let i = 0; i < 5; i++) {
     content.push(
-      <tr>
-        <td>_______________ - </td>
-        <td>_______________ =</td>
-        <td>_____________</td>
-      </tr>
+      <>
+        <p>_______________ - </p>
+        <p>_______________ =</p>
+        <p>_____________</p>
+      </>
     );
   }
 
   return (
     <>
-      <div class="pdf-page">
-        <div class="row">
-          <div class="col-50">
-            {informations.map((item) => {
-              return (
-                <>
-                  <div id="card">
-                    <div className="logo">
-                      <Row>
-                        <Col span={12}>
-                          <img src={logo} style={{ height: '45px' }} />
-                        </Col>
+      <div className="wmsTags">
+        <span style={{ background: 'white' }}>
+          <div className="logo">
+            <Row>
+              <Col span={12} align={'left'}>
+                <img src={logo} style={{ height: '45px' }} />
+              </Col>
 
-                        <Col span={12}>
-                          <h4>Entrada realizada em:</h4>
-                          <h3>{item.date_entry}</h3>
-                        </Col>
-                      </Row>
-                    </div>
-
-                    <Divider style={{ marginTop: '30px' }} />
-
-                    <div className="table-header">
-                      <table width="90%" style={{ marginLeft: '5%' }}>
-                        <tr>
-                          <th>Almoxarifado</th>
-                          <th>Posição</th>
-                        </tr>
-                        <tr>
-                          <td>{item.warehouse}</td>
-                          <td>{item.street}</td>
-                        </tr>
-                      </table>
-                    </div>
-
-                    <div className="table-header">
-                      <table
-                        width="90%"
-                        style={{ marginLeft: '5%', marginTop: '10px' }}
-                      >
-                        <tr>
-                          <th>Insumo</th>
-                          <th>Quantidade</th>
-                        </tr>
-                        <tr>
-                          <td>{item.ins_name}</td>
-                          <td>{item.quantity}</td>
-                        </tr>
-                      </table>
-                    </div>
-                    <div className="ins">
-                      <h3 style={{ marginTop: '15px' }}>{item.ins}</h3>
-                    </div>
-
-                    <Divider style={{ marginTop: '5px' }} />
-
-                    <div className="content-table">
-                      <table width="90%" style={{ margin: '0' }}>
-                        <tr>
-                          <th>QTDE ATUAL</th>
-                          <th>QTDE SAÍDA</th>
-                          <th>RESULTADO</th>
-                        </tr>
-                        {content}
-                      </table>
-                    </div>
-
-                    <Divider style={{ marginTop: '8px' }} />
-
-                    <div className="barcode">
-                      <BarCode
-                        value={item.barcode}
-                        width={1.2}
-                        height={40}
-                        fontSize={20}
-                      />
-                    </div>
-                  </div>
-                </>
-              );
-            })}
+              <Col span={12}></Col>
+            </Row>
           </div>
-        </div>
+          <Divider style={{ marginTop: '30px' }} />
+          <div className="wmsTagBody">
+            <div className="firstColum">
+              <b>Almoxarifado</b>
+              <p>pedro</p>
+              <b>Insumo</b>
+              <p>Nome</p>
+            </div>
+            <div className="secondColum">
+              <b>Posição</b>
+              <p>pedro</p>
+              <b>Posição</b>
+              <p>Quantidade</p>
+            </div>
+          </div>
+          <Divider style={{ marginTop: '5px' }} />
+
+          <div className="operations">
+            <p>QTDE ATUAL</p>
+            <p>QTDE SAÍDA</p>
+            <p>RESULTADO</p>
+
+            {content}
+          </div>
+          <Divider style={{ marginTop: '8px' }} />
+
+          <div className="wmsTagBarCode">
+            <BarCode value={'12abhhf'} width={1.2} height={40} fontSize={20} />
+          </div>
+        </span>
       </div>
     </>
   );
