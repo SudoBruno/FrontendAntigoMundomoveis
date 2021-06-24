7dcd9ca9c6d9f8188ef73da0c6da2a6fa3ff6bca
phdgamer <pedro380dalpa@gmail.com>
2021-06-24 16:07:35 -0300

    remove @createnextapp/react-barcode

--- src/components/MachineTag/index.js
+++ src/components/MachineTag/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 import { useParams } from 'react-router-dom';
-import { useBarcode } from '@createnextapp/react-barcode';
 
 import api from '../../services/api';
 import './style.css';
