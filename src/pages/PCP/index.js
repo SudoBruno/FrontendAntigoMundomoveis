aa7cdaaa2667f198dd2d8947ac395709c28fe613
SudoBruno <brunoleonardo.profissional@gmail.com>
2021-07-26 17:19:03 -0300

    concertando erro

--- src/pages/PCP/index.js
+++ src/pages/PCP/index.js
@@ -661,7 +661,7 @@ export default function PCP() {
                               key={option.id}
                               value={[option.id, option.name]}
                             >
-                              { product.code + option.name }
+                              { option.code + option.name }
                             </Option>
                           </>
                         );
